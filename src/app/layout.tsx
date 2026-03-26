4c78436c2b55a5619d00f8b307a9ed8787a165b1
dondahari <125116972+dondahari@users.noreply.github.com>
2026-03-26 00:29:06 -0400

    Branding: Setup favicons and update layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'UniLink-App',
   description: 'Connecting Students with Employers Seamlessly.',
+  icons: {
+    icon: [
+      { url: '/favicons/favicon.ico' },
+      { url: '/favicons/icon1.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicons/icon0.svg', type: 'image/svg+xml' },
+    ],
+    apple: [
+      { url: '/favicons/apple-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/favicons/manifest.json',
 };
 
 export default function RootLayout({
